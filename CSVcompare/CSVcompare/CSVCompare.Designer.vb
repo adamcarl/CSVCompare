1bfd45e20ff68a9cfca01c41f40b8277b451ed86
Adam Medina <adamcarlmedina@gmail.com>
2017-08-14 10:25:50 +0800

    Demo
    Ready for demo

---
+++ CSVcompare/CSVcompare/CSVCompare.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CSVCompare
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ImportFromPOSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImportFromDCToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.ExportResultToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImportFromPOSToolStripMenuItem, Me.ImportFromDCToolStripMenuItem, Me.ExportResultToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(750, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ImportFromPOSToolStripMenuItem
+        '
+        Me.ImportFromPOSToolStripMenuItem.Name = "ImportFromPOSToolStripMenuItem"
+        Me.ImportFromPOSToolStripMenuItem.Size = New System.Drawing.Size(109, 20)
+        Me.ImportFromPOSToolStripMenuItem.Text = "Import from POS"
+        '
+        'ImportFromDCToolStripMenuItem
+        '
+        Me.ImportFromDCToolStripMenuItem.Name = "ImportFromDCToolStripMenuItem"
+        Me.ImportFromDCToolStripMenuItem.Size = New System.Drawing.Size(103, 20)
+        Me.ImportFromDCToolStripMenuItem.Text = "Import from DC"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(8, 72)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(350, 300)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Refresh"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Items with match."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(382, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "All Items"
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.AllowUserToAddRows = False
+        Me.DataGridView3.AllowUserToDeleteRows = False
+        Me.DataGridView3.AllowUserToOrderColumns = True
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(385, 72)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.ReadOnly = True
+        Me.DataGridView3.Size = New System.Drawing.Size(350, 300)
+        Me.DataGridView3.TabIndex = 6
+        '
+        'ExportResultToolStripMenuItem
+        '
+        Me.ExportResultToolStripMenuItem.Name = "ExportResultToolStripMenuItem"
+        Me.ExportResultToolStripMenuItem.Size = New System.Drawing.Size(87, 20)
+        Me.ExportResultToolStripMenuItem.Text = "Export Result"
+        '
+        'CSVCompare
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(750, 411)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DataGridView3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "CSVCompare"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CSVCompare"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ImportFromPOSToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImportFromDCToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents ExportResultToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ CSVcompare/CSVcompare/CSVCompare.vb
@@ -0,0 +1,166 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+Imports ADODB
+Imports System.Windows.Forms.Application
+Imports ADOX
+
+Public Class CSVCompare
+    Dim tablePOS As String = "PosData"
+    Dim tableDC As String = "CollectorData"
+    Dim ExcelPath As String
+    Dim connTable As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & StartupPath & "\Database\inOut.mdb")
+    Dim conn As New Connection
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            connTable.Open()
+        Catch ex As Exception
+            ' Part 1: Create Access Database file using ADOX
+            If (Not Directory.Exists(StartupPath & "/Database")) Then
+                Directory.CreateDirectory(StartupPath & "/Database")
+            End If
+            Try
+                Call New Catalog().Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & StartupPath & "\Database\inOut.mdb;Jet OLEDB:Engine Type=5")
+            Catch exe As Exception
+
+            End Try
+
+            ' Part 2: Create one Table using OLEDB Provider 
+            'Get database schema
+            connTable.Open()
+            Dim dbSchema As DataTable = connTable.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, (New Object() {Nothing, Nothing, tablePOS, "TABLE"}))
+            connTable.Close()
+
+            ' If the table exists, the count = 1
+            If dbSchema.Rows.Count > 0 Then
+                ' do whatever you want to do if the table exists
+            Else
+                'do whatever you want to do if the table does not exist
+                ' e.g. create a table
+                connTable.Open()
+                Call New OleDbCommand("CREATE TABLE [" + tablePOS + "] ([Id] COUNTER, [DataCode] TEXT(50), [DataDescription] TEXT(100))", connTable).ExecuteNonQuery()
+                Call New OleDbCommand("CREATE TABLE [" + tableDC + "] ([Id] COUNTER, [DataCode] TEXT(50))", connTable).ExecuteNonQuery()
+            End If
+        End Try
+        connTable.Close()
+    End Sub
+
+    Private Sub ImportFromPOSToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImportFromPOSToolStripMenuItem.Click
+        connTable.Open()
+
+        Dim OpenFileDialog1 As New OpenFileDialog With {
+            .Filter = "Excel Files (*)|*.csv;"
+        }
+
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            ExcelPath = OpenFileDialog1.FileName
+            Try
+                ClearTablePos()
+                Call New OleDbCommand($"INSERT INTO [PosData] SELECT F1 AS DataCode, F2 AS DataDescription FROM [Text;FMT=Delimited;HDR=No;CharacterSet=850;DATABASE={Path.GetDirectoryName(ExcelPath)}].[{ExcelPath.Split("\")(ExcelPath.Split("\").Length - 1)}]", connTable).ExecuteNonQuery()
+                MessageBox.Show("Import Successful.")
+            Catch ex As Exception
+                MessageBox.Show("Import Failure.")
+                'MessageBox.Show(ex.ToString)
+            End Try
+        End If
+        connTable.Close()
+        Call DataGridShow()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call DataGridShow()
+    End Sub
+
+    Private Sub ExportResultToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportResultToolStripMenuItem.Click
+        subExportDGVToCSV($"CompareResult_{Date.Now.ToString("yyyy-MM-dd HH-mm-ss")}.csv", DataGridView3)
+        Process.Start(StartupPath)
+    End Sub
+
+    Private Sub ImportFromDCToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImportFromDCToolStripMenuItem.Click
+        connTable.Open()
+        Dim OpenFileDialog1 As New OpenFileDialog With {
+            .Filter = "Excel Files (*)|*.csv;"
+        }
+
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            ExcelPath = OpenFileDialog1.FileName
+            Try
+                ClearTableCollector()
+                Call New OleDbCommand("INSERT INTO [CollectorData] SELECT F1 AS DataCode FROM [Text;FMT=Delimited;HDR=No;CharacterSet=850;DATABASE=" & Path.GetDirectoryName(ExcelPath) & "].[" & ExcelPath.Split("\")(ExcelPath.Split("\").Length - 1) & "]", connTable).ExecuteNonQuery()
+                MessageBox.Show("Import Successful.")
+            Catch ex As Exception
+                MessageBox.Show("Import Failure.")
+                'MessageBox.Show(ex.ToString)
+            End Try
+        End If
+        connTable.Close()
+        Call DataGridShow()
+    End Sub
+    Sub ClearTablePos()
+        Try
+            Call New OleDbCommand("DELETE * FROM PosData", connTable).ExecuteNonQuery()
+        Catch ex As Exception
+            'MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Sub ClearTableCollector()
+        Try
+            Call New OleDbCommand("DELETE * FROM CollectorData", connTable).ExecuteNonQuery()
+        Catch ex As Exception
+            'MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub DataGridShow()
+        connTable.Open()
+        Dim dt1 As New DataTable
+        'Dim dt2 As New DataTable
+        Dim dt3 As New DataTable
+
+        Call New DataSet().Tables.Add(dt1)
+        'Call New DataSet().Tables.Add(dt2)
+        Call New DataSet().Tables.Add(dt3)
+
+        'da = New OleDbDataAdapter("SELECT [PosData.DataDescription],[PosData.DataCode],[CollectorData.DataCode] FROM [PosData] INNER JOIN [CollectorData] ON [PosData.DataCode] = [CollectorData.DataCode]", connTable)
+
+        Call New OleDbDataAdapter("SELECT [PosData.DataDescription] AS Name,[PosData.DataCode] AS Pos,[CollectorData.DataCode] AS Collector FROM [PosData] INNER JOIN [CollectorData] ON [PosData].DataCode LIKE [CollectorData].DataCode", connTable).Fill(dt1)
+        DataGridView1.DataSource = dt1.DefaultView
+
+        ''Call New OleDbDataAdapter("SELECT [PosData.DataDescription] AS Name,[PosData.DataCode] AS Pos,[CollectorData.DataCode] AS Collector FROM [PosData] INNER JOIN [CollectorData] ON [PosData].DataCode NOT LIKE [CollectorData].DataCode AND [CollectorData].DataCode NOT LIKE [PosData].DataCode", connTable).Fill(dt2)
+        'Call New OleDbDataAdapter("SELECT [PosData.DataDescription] AS Name,[PosData.DataCode] AS Pos,[CollectorData.DataCode] AS Collector FROM [PosData] LEFT JOIN [CollectorData] ON [PosData].DataCode LIKE [CollectorData].DataCode", connTable).Fill(dt2)
+        'DataGridView2.DataSource = dt2.DefaultView
+
+        Call New OleDbDataAdapter("SELECT [PosData.DataDescription] AS Name,[PosData.DataCode] AS Pos,[CollectorData.DataCode] AS Collector FROM [PosData] LEFT JOIN [CollectorData] ON [PosData].DataCode LIKE [CollectorData].DataCode UNION SELECT [PosData.DataDescription] AS Name,[PosData.DataCode] AS Pos,[CollectorData.DataCode] AS Collector FROM [CollectorData] LEFT JOIN [PosData] ON [PosData].DataCode LIKE [CollectorData].DataCode", connTable).Fill(dt3)
+        DataGridView3.DataSource = dt3.DefaultView
+
+        connTable.Close()
+    End Sub
+    Private Sub subExportDGVToCSV(ByVal strExportFileName As String, ByVal DataGridView As DataGridView, Optional ByVal blnWriteColumnHeaderNames As Boolean = False, Optional ByVal strDelimiterType As String = ",")
+
+        Dim sr As StreamWriter = File.CreateText(strExportFileName)
+        Dim strDelimiter As String = strDelimiterType
+        Dim intColumnCount As Integer = DataGridView.Columns.Count - 1
+        Dim strRowData As String = ""
+
+
+        For intX As Integer = 0 To intColumnCount
+                strRowData += Replace(DataGridView.Columns(intX).Name, strDelimiter, "") & IIf(intX < intColumnCount, strDelimiter, "")
+            Next intX
+            sr.WriteLine(strRowData)
+
+        For intX As Integer = 0 To DataGridView.Rows.Count - 1
+            strRowData = ""
+            For intRowData As Integer = 0 To intColumnCount
+                Try
+                    strRowData += Replace(DataGridView.Rows(intX).Cells(intRowData).Value, strDelimiter, "") & IIf(intRowData < intColumnCount, strDelimiter, "")
+                Catch ex As Exception
+                    strRowData += ","
+                End Try
+            Next intRowData
+            sr.WriteLine(strRowData)
+        Next intX
+        sr.Close()
+    End Sub
+End Class
